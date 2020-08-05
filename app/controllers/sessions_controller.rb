e31eb83799af4d82f41547d336adfb95ae45a98a
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-08-05 11:24:36 -0400

    built out authentication, sign up, log in, log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,27 @@ class SessionsController < ApplicationController
     erb :'users/login'
   end
 
+  post '/login' do 
+    if params["username"].empty? || params["password"].empty?
+      @error = "Please fill in both username and password"
+      erb :'users/login'
+    else
+      if user = User.find_by(username: params["username"], password: params["password"]) 
+        session[:user_id] = user.id
+        redirect '/lists'
+      else
+        @error = "Account not found. Please try again."
+        erb :'users/login'
+      end 
+    end       
+  end
+
   # users can log out
+
+  get '/logout' do 
+    session.clear 
+    redirect '/'
+  end
 
 
 end
