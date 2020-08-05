f1563584dd6c0d3ac65f5d28adec553b378da245
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-08-04 21:47:52 -0400

    worked on Create/Read/Update, Edit View

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,7 +18,7 @@ class ListsController < ApplicationController
         list.save
         redirect '/lists'
       else 
-        @error = "Data invalid. Please make sure to enter text into both fields."
+        @error = "Please make sure to enter text into all fields."
         erb :'/lists/new'
       end
     end
@@ -30,7 +30,7 @@ class ListsController < ApplicationController
     # make a get request to '/lists'
   
     get '/lists' do 
-      @lists = List.all
+      @lists = List.all.reverse
       erb :'lists/index' 
     end
 
@@ -38,7 +38,7 @@ class ListsController < ApplicationController
     # make a get request to '/lists/:id'
 
     get '/lists/:id' do 
-      @list = List.find(params["id"])
+      @list = List.find(params[:id])
       erb :'lists/show'
     end
 
@@ -48,10 +48,24 @@ class ListsController < ApplicationController
 
     # Edit
     # make a get request to '/lists/:id/edit'
+    get '/lists/:id/edit' do 
+      @list = List.find(params[:id])
+      erb :'/lists/edit'
+    end
+
 
     # Update
     # make a patch request to '/lists/:id'
-
+    patch '/lists/:id' do 
+      @list = List.find(params[:id])
+      if !params["list"]["title"].empty? && !params["list"]["notes"].empty?
+        @list.update(params["list"]) 
+        redirect "/lists/#{params[:id]}"
+      else 
+        @error = "Please make sure to enter text into all fields."
+        erb :'/lists/edit'
+      end 
+    end
 
 
   # DESTROY
